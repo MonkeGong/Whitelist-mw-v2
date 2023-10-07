ecf2fdab7f3c00014184f0dee4cf4494322346af
RandomUserRay <145318415+RandomUserRay@users.noreply.github.com>
2023-10-07 18:54:30 +0700

    Add files via upload

---
+++ Whitelisted Mystic.lua
@@ -0,0 +1,40 @@
+
+
+
+{
+  "Owner": [
+    {"id": "5071878111"},
+    {"id": "5083508263"},
+    {"id": "11"}
+],
+  "Private": [
+    {"id": "1"},
+    {"id": "1"},
+    {"id": "1"},
+    {"id": "1"},
+    {"id": "1"},
+    {"id": "1"},
+    {"id": "11"},
+    {"id": "11"},
+    {"id": "11"},
+    {"id": "11"},
+    {"id": "11"},
+    {"id": "11"},
+    {"id": "11"},
+    {"id": "11"}
+],
+  "tags": [
+    {"userid": "5071878111", "tag": "RAYUWU💜", "color": "pink"},
+    {"userid": "5083508263", "tag": "MYSTICWARE RED", "color": "purple"},
+    {"userid": "1", "tag": "nn", "color": "purple"},
+    {"userid": "1", "tag": "nn", "color": "purple"},
+    {"userid": "1", "tag": "nn", "color": "purple"},
+    {"userid": "18", "tag": "nn", "color": "yellow"},
+    {"userid": "19", "tag": "nn", "color": "cyan"},
+    {"userid": "20", "tag": "nn", "color": "purple"},
+    {"userid": "21", "tag": "nn", "color": "pink"},
+    {"userid": "22", "tag": "nn", "color": "purple"},
+    {"userid": "23", "tag": "nn", "color": "purple"},
+    {"userid": "24", "tag": "nn", "color": "red"}
+  ]
+}
