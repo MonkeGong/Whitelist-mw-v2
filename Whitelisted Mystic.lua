05cb8135f4bb77db7cffe38e279b64dd41d81f82
RandomUserRay <145318415+RandomUserRay@users.noreply.github.com>
2023-10-09 12:16:22 +0700

    Update Whitelisted Mystic.lua

--- Whitelisted Mystic.lua
+++ Whitelisted Mystic.lua
@@ -26,7 +26,7 @@
   "tags": [
     {"userid": "5071878111", "tag": "RAYUWUFEMBOY💜", "color": "pink"},
     {"userid": "5083508263", "tag": "MYSTICWARE OWNER", "color": "purple"},
-    {"userid": "5100615950", "tag": "VAPE PRIVATE", "color": "purple"},
+    {"userid": "5100615950", "tag": "MYSTICWARE PRIVATE", "color": "purple"},
     {"userid": "5065312181", "tag": "CoOwner", "color": "purple"},
     {"userid": "1", "tag": "nn", "color": "black"},
     {"userid": "18", "tag": "nn", "color": "yellow"},
