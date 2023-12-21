c19e66b4c0d56534a6f4cd62cf764501c5923317
MonkeGong <139249700+MonkeGong@users.noreply.github.com>
2023-12-21 21:05:26 +0200

    Update Whitelisted Mystic.lua

--- Whitelisted Mystic.lua
+++ Whitelisted Mystic.lua
@@ -3,6 +3,6 @@ print("uh oh? still mad? pipe down")
 local clown = "stav"
 local obsessed = "yup, still stav"
 
-if clown == obsessed then
+if clown ~= obsessed then
 print("yup you're a clown")
 end
